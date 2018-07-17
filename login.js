2cdcf1cf9743f60a281edf1e7e448ff31aa92c13
shreyaskargit <shreyaskar30@gmail.com>
2018-07-18 00:50:35 +0530

    login page

---
+++ login.js
@@ -0,0 +1,9 @@
+var sign = document.querySelector("#sign");
+var login = document.querySelector("#login");
+
+sign.addEventListener("click", function(){
+	document.location.href = 'formquiz.html';
+});
+login.addEventListener("click", function(){
+	document.location.href = 'sign.html';
+});
